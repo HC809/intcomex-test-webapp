0920683be3a812c7897e8c505a705cd99453e3e3
Hector Caballero <caballero.22.809@gmail.com>
2022-12-01 12:53:08 -0600

    Add GenericRepository & UoW with async methods

---
+++ intcomex-test-webapp.DAL/UnitOfWork.cs
@@ -0,0 +1,37 @@
+﻿using intcomex_test_webapp.DL.IntcomexDBContext;
+using Microsoft.EntityFrameworkCore.Storage;
+
+namespace intcomex_test_webapp.DAL
+{
+    public class UnitOfWork : IAsyncDisposable
+    {
+        private IntcomexDbContext context = new IntcomexDbContext();
+        private IDbContextTransaction objTran;
+
+        public async Task<int> SaveAsync() => await context.SaveChangesAsync();
+        public async Task<IDbContextTransaction> CreateTransactionAsync() => objTran = await context.Database.BeginTransactionAsync();
+        public async Task CommitAsync() => await objTran.CommitAsync();
+        public async Task RollbackAsync()
+        {
+            await objTran.RollbackAsync();
+            await objTran.DisposeAsync();
+        }
+
+        private bool disposed = false;
+
+        public async ValueTask DisposeAsync()
+        {
+            await DisposeAsync(true);
+            GC.SuppressFinalize(this);
+        }
+
+        protected virtual async ValueTask DisposeAsync(bool disposing)
+        {
+            if (!this.disposed)
+            {
+                if (disposing) await context.DisposeAsync();
+                this.disposed = true;
+            }
+        }
+    }
+}
